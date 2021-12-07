509b3e9a291646c416dcc08c8df99a8dfd2cdd18
joshua360x <jkw5499@psu.edu>
2021-12-07 18:57:23 -0500

    fixed esLint errors

--- render-Where.js
+++ render-Where.js
@@ -2,7 +2,7 @@
 export function renderWhereFromFunction(whereData) {
     const div = document.createElement('div');
     const paragraph = document.createElement('p');
-    const p = document.createElement('p');
+    // const p = document.createElement('p');
 
     div.classList.add('whereFrom');
 
@@ -10,7 +10,7 @@ export function renderWhereFromFunction(whereData) {
 //   console.log(homeTown.whereFrom);
 //   p.textContent = 
 // }
-    paragraph.textContent = `My name is ${whereData.name} and I live in ${whereData.livesIn}, From ${whereData.whereFrom.city},${whereData.whereFrom.state}`;
-    div.append(paragraph, p);
+    paragraph.textContent = `My name is ${whereData.name} and I live in ${whereData.livesIn}, originally From ${whereData.whereFrom.city},${whereData.whereFrom.state}`;
+    div.append(paragraph);
     return div;
 }
